cc78d47c99c81636575850aceed47c65b5d1c5e7
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-02-01 16:51:43 +0530

    client

--- law/src/Component/Clients/Clients.jsx
+++ law/src/Component/Clients/Clients.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-
-import Cards from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import style from "./Clients.module.css";
-import Avatar from "@mui/material/Avatar";
+
 import client1 from "../../Assests/client1.png";
 import client2 from "../../Assests/client2.png";
 import client3 from "../../Assests/client3.png";
@@ -16,73 +12,59 @@ const Clients = () => {
         <h1>What says our</h1>
         <h1> happy Clients</h1>
       </div>
-
       <div className={style.rapper}>
         <div className={style.square}>
-          <Cards style={{ backgroundColor: "transparent" }}>
+          <div>
             <img src={client1} alt="Jane Cooper" className={style.img} />
+          </div>
+          <div className={style.name}>
+            <h4>Jane Cooper</h4>
+          </div>
+          <div className={style.company}>
+            <h5> Ceo of Hunt</h5>
+          </div>
 
-            <CardContent className={style.content}>
-              <Typography variant="h5" className={style.name}>
-                Jane Cooper
-              </Typography>
-              <Typography
-                variant="h7"
-                component="div"
-                className={style.company}
-              >
-                Ceo of Hunt
-              </Typography>
-              <Typography className={style.paragraph}>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequatduis enim velit mollit Exer.
-              </Typography>
-            </CardContent>
-          </Cards>
+          <p className={style.paragraph}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua
+            dolor do amet sint. Velit officia
+          </p>
         </div>
-        <div className={style.newSqur}>
-          <Cards style={{ backgroundColor: "#2E2E2E" }}>
-            <img src={client3} alt="Jane Cooper" className={style.img} />
 
-            <CardContent>
-              <Typography variant="h5" className={style.name}>
-                Devon Lane
-              </Typography>
-              <Typography
-                variant="h7"
-                component="div"
-                className={style.company}
-              >
-                Ceo of Hunt
-              </Typography>
-              <Typography className={style.paragraph}>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequatduis enim velit mollit Exer.
-              </Typography>
-            </CardContent>
-          </Cards>
+        <div className={style.newSqur}>
+          <div>
+            <img src={client3} alt="Jane Cooper" className={style.img} />
+          </div>
+          <div className={style.name}>
+            <h4>Devon Lane</h4>
+          </div>
+          <div className={style.company}>
+            <h5> Ceo of Hunt</h5>
+          </div>
+
+          <p className={style.paragraph}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua
+            dolor do amet sint. Velit officia
+          </p>
         </div>
 
         <div className={style.square}>
-          <Cards style={{ backgroundColor: "transparent" }}>
-            <img src={client2} alt="Robert Fox" className={style.img} />
-            <CardContent>
-              <Typography variant="h5" className={style.name}>
-                Robert Fox
-              </Typography>
-              <Typography
-                variant="h7"
-                component="div"
-                className={style.company}
-              >
-                Ceo of Hunt
-              </Typography>
-              <Typography className={style.paragraph}>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequatduis enim velit mollit Exer.
-              </Typography>
-            </CardContent>
-          </Cards>
+          <div>
+            <img src={client2} alt="Jane Cooper" className={style.img} />
+          </div>
+          <div className={style.name}>
+            <h4>Robert Fox</h4>
+          </div>
+          <div className={style.company}>
+            <h5> Ceo of Hunt</h5>
+          </div>
+
+          <p className={style.paragraph}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequatduis enim velit mollit Exer. sit aliqua
+            dolor do amet sint. Velit officia
+          </p>
         </div>
       </div>
     </div>
