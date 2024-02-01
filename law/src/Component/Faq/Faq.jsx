ed904ac8eccef24193b2adfb971572d94b5eaa87
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-02-01 17:49:38 +0530

    FINAL

--- law/src/Component/Faq/Faq.jsx
+++ law/src/Component/Faq/Faq.jsx
@@ -5,6 +5,7 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import style from "./Faq.module.css";
 import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import Line from "../../Assests/Line1.png";
 
 const Faq = () => {
   return (
@@ -18,57 +19,65 @@ const Faq = () => {
       </div>
       <div className={style.accor}>
         <Accordion defaultExpanded>
-          <AccordionSummary aria-controls="panel1-content" id="panel1-header">
+          <AccordionSummary
+            aria-controls="panel1-content"
+            id="panel1-header"
+            className={style.acc}
+          >
             Do I need a personal injury report?
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails className={style.content}>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequatduis enim velit mollit Exer. Amet minim
             mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
             officia consequatduis enim velit mollit Exer.
           </AccordionDetails>
         </Accordion>
+
         <Accordion>
           <AccordionSummary
             expandIcon={<AddCircleRoundedIcon style={{ color: "#E3B748" }} />}
             aria-controls="panel1-content"
             id="panel1-header"
+            className={style.acc}
           >
             How much is my case worth?
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails className={style.content}>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequatduis enim velit mollit Exer. Amet minim
             mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
             officia consequatduis enim velit mollit Exer.
           </AccordionDetails>
         </Accordion>
-        {/* <hr /> */}
+        <img src={Line} alt="" />
         <Accordion>
           <AccordionSummary
             expandIcon={<AddCircleIcon style={{ color: "#E3B748" }} />}
             aria-controls="panel1-content"
             id="panel1-header"
+            className={style.acc}
           >
             What should I do right after car accidect
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails className={style.content}>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequatduis enim velit mollit Exer. Amet minim
             mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
             officia consequatduis enim velit mollit Exer.
           </AccordionDetails>
         </Accordion>
-        {/* <hr /> */}
+
         <Accordion>
           <AccordionSummary
             expandIcon={<AddCircleIcon style={{ color: "#E3B748" }} />}
             aria-controls="panel1-content"
             id="panel1-header"
+            className={style.acc}
           >
             What should I do right after car accidect
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails className={style.content}>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequatduis enim velit mollit Exer. Amet minim
             mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
